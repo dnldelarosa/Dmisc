42c44c54eaa251b39ca544ab467764c08b6dc917
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-01-02 17:04:22 -0400

    feat: new Sys_Date_locale function

--- NEWS.md
+++ NEWS.md
@@ -1,16 +1,14 @@
-# Dmisc 0.1.1.9000
+# Dmisc 0.1.1
 
 * Added a `NEWS.md` file to track changes to the package.
 * Added condition for converting images to base64 string. MatLab Live Scripts does this by default.
-
-# Dmisc 0.1.1.9001
-
 * Fixed backward incompatibility added in previous mod
-
-# Dmisc 0.1.1.9002
-
 * Set current path as default path insertHTML function
 
-# Dmisc 0.1.2.9002
+# Dmisc 0.1.2
 
 * New vars_to_date function
+
+# Dmisc 0.1.3
+
+* New Sys_Date_locale function
---
+++ R/Sys_Date_locale.R
@@ -0,0 +1,22 @@
+#' System date with specific format and locale
+#'
+#'  You need to have installed the specified locale.
+#'
+#' @param locale a currently installed locale string
+#' @param format a valid format for dates or NULL
+#'
+#' @return a date with specified locale and format
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  Sys_Date_locale()
+#' }
+Sys_Date_locale <- function(locale = "es_DO.utf8", format = "%a de %B de %Y"){
+  if(is.null(format)){
+    withr::with_locale(new = c("LC_TIME" = locale), Sys.Date())
+  } else {
+    withr::with_locale(new = c("LC_TIME" = locale), format(Sys.Date()))
+  }
+}
