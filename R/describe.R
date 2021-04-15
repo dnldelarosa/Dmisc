75ba5bc9cb3bc04ff53f724434ac24d6b4030098
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-04-15 09:23:25 -0400

    feat: describe accept additional arguments for flextable object

--- R/describe.R
+++ R/describe.R
@@ -1,9 +1,13 @@
 #' Create a table of summaries for an entery dataframe
 #'
+#'   \lifecycle{stable}
+#'
 #' @param data [data.frame]: a dataframe with the data
 #' @param digits [numeric]: number of digits for the numbers
 #' @param t [logical]: indicates if the resulting table is transposed
-#' @param flextable [logical]: indicates if the table is converted to a simple flextable
+#' @param flextable [logical]: indicates if the table is converted to a simple flextable.
+#'    See \code{base::\link[flextable:flextable]{flextable}}
+#' @param ft_args [list]: other arguments passed to \code{base::\link[flextable:flextable]{flextable}} function
 #' @param ... other arguments passed to summary function
 #'
 #' @return a table containing the summary of every variable in the provided dataframe
@@ -13,8 +17,9 @@
 #' @examples
 #' \dontrun{
 #' describe(cars, flextable = T)
+#' describe(cars, flextable = T, ft_args = list(cwidth = 1))
 #' }
-describe <- function(data, digits = 4, t = TRUE, flextable = FALSE, ...){
+describe <- function(data, digits = 4, t = TRUE, flextable = FALSE, ft_args = list(), ...){
   res <- list()
   for (variable in seq_along(data)) {
     res[[names(data)[[variable]]]] <- summary(data[[variable]], digits = digits, ...)
@@ -30,9 +35,15 @@ describe <- function(data, digits = 4, t = TRUE, flextable = FALSE, ...){
     }
     res <- utils::type.convert(res)
     if(flextable){
+      if(length(ft_args) > 0){
+        res <- res %>%
+          tibble::rownames_to_column(" ")
+        res <- do.call(flextable::flextable, c(list(data = res), ft_args))
+      } else {
       res <- res %>%
         tibble::rownames_to_column(" ") %>%
         flextable::flextable()
+      }
     }
     res
 }
