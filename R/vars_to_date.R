760707ed61a6aea43f25413f480ad0e5819d80dd
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-06-07 10:14:13 -0400

    feat: quarterly date

--- R/vars_to_date.R
+++ R/vars_to_date.R
@@ -38,7 +38,6 @@ vars_to_date <- function(tbl,
                          date = NULL,
                          date_format = "%d-%m-%y",
                          origin = "1900-01-01") {
-
   if (lifecycle::is_present(trim)) {
     deprecate_warn("0.2.4", "Dmisc::vars_to_date(trim = )", "Dmisc::vars_to_date(quarter = )")
   }
@@ -47,7 +46,7 @@ vars_to_date <- function(tbl,
     deprecate_warn("0.2.4", "Dmisc::vars_to_date(month_type = )")
   }
 
-  if(all(is.null(year), is.null(date))){
+  if (all(is.null(year), is.null(date))) {
     stop("One of 'year' or 'date' variable needs to be non-null")
   }
 
@@ -86,128 +85,159 @@ vars_to_date <- function(tbl,
     names(tbl)[day] <- "day"
   }
 
-  if(!is.null(quarter)){
-    return(qdate(tbl))
+  if (!is.null(date)) {
+    tbl <- clean_date(tbl)
   }
 
   if (all("month" %in% names(tbl), !is.numeric(tbl[["month"]]))) {
-    tbl <- dplyr::mutate(tbl,
-      month = stringr::str_trim(month),
-      month = stringr::str_sub(month, 1, 3),
-      month = stringr::str_to_title(month),
-      month = dplyr::case_when(
-        month == "Ene" ~ "Jan",
-        month == "Abr" ~ "Apr",
-        month == "Ago" ~ "Aug",
-        month == "Dic" ~ "Dec",
-        TRUE ~ month
-      )
-    )
+    tbl <- abb_months(tbl)
   }
 
-  if (!is.null(date)) {
-    tbl <- tbl %>%
-      dplyr::mutate(
-        date = dplyr::case_when(
-          is.na(as.numeric(date)) ~ stringr::str_remove_all(stringr::str_remove_all(date, "\\."), "\\*"),
-          TRUE ~ date
-        ),
-        date = dplyr::case_when(
-          stringr::str_detect(tolower(date), "enero") ~ stringr::str_replace(tolower(date), "enero", "jan"),
-          stringr::str_detect(tolower(date), "ene") ~ stringr::str_replace(tolower(date), "ene", "jan"),
-          stringr::str_detect(tolower(date), "abril") ~ stringr::str_replace(tolower(date), "abril", "apr"),
-          stringr::str_detect(tolower(date), "abr") ~ stringr::str_replace(tolower(date), "abr", "apr"),
-          stringr::str_detect(tolower(date), "agosto") ~ stringr::str_replace(tolower(date), "agosto", "aug"),
-          stringr::str_detect(tolower(date), "ago") ~ stringr::str_replace(tolower(date), "ago", "aug"),
-          stringr::str_detect(tolower(date), "diciembre") ~ stringr::str_replace(tolower(date), "diciembre", "dec"),
-          stringr::str_detect(tolower(date), "dic") ~ stringr::str_replace(tolower(date), "dic", "dec"),
-          TRUE ~ date
-        ),
-        date = dplyr::case_when(
-          stringr::str_detect(
-            date,
-            stringr::regex("[a-z]{3}-")
-            ) ~ stringr::str_replace(
-              date,
-              stringr::regex("[a-z]{3}"),
-              paste0(
-                match(
-                  stringr::str_to_title(stringr::str_extract(date, "[a-z]{3}")),
-                  month.abb
-                )
-              )
-            ),
-          TRUE ~ date
-        )
-      )
-    if(date_format %in% c("%d-%m-%y", "%m-%y")){
-      tbl <- tbl %>%
-        dplyr::mutate(
-          date = dplyr::case_when(
-            stringr::str_count(date, "-") == 1 & stringr::str_starts(date, "[0-9]") ~ paste0(1, "-", date),
-            TRUE ~ date
-          ),
-          date = dplyr::case_when(
-            stringr::str_count(date, "-") == 2 ~ as.character(difftime(as.Date(date, format = date_format), as.Date(origin), units = "days")),
-            TRUE ~ date
-          )
-        )
+  if ("year" %in% names(tbl)) {
+    if ("day" %in% names(tbl)) {
+      tbl <- ddate(tbl)
+    } else if (!is.null(month)) {
+      tbl <- mdate(tbl)
+    } else if (!is.null(quarter)) {
+      tbl <- qdate(tbl)
     }
-    tbl <- tbl %>%
-      dplyr::mutate(
-        date = as.Date(as.numeric(date), origin = origin)
-      )
   } else {
-    if ("year" %in% names(tbl)) {
-      tbl[["date"]] <- tbl[["year"]]
-      if ("day" %in% names(tbl)) {
-        if (!is.numeric(tbl[["month"]])) {
-          tbl <- dplyr::mutate(
-            tbl,
-            month = match(month, month.abb)
-          )
-        }
-        tbl <- dplyr::mutate(tbl,
-          date = paste(year, month, day, sep = "-")
-        )
-      } else if ("month" %in% names(tbl)) {
-        if (is.numeric(tbl$month)) {
-          tbl <- dplyr::mutate(tbl,
-            date = paste(year, month, "01", sep = "-")
-          )
-        } else {
-          if (!requireNamespace("tsibble", quietly = TRUE)) {
-            stop("Package \"tsibble\" needed for yearmonth format work. Please install it.", call. = FALSE)
-          }
-          tbl <- dplyr::mutate(
-            tbl,
-            date = as.Date(tsibble::yearmonth(paste(year, month)))
-          )
-        }
-      }
-    } else {
-      stop("Please. Specifies the position or name of the year variable.")
-    }
+    stop("Argument vars_to_date(year = ) is required.")
   }
+
   tbl[["day"]] <- NULL
   tbl[["month"]] <- NULL
+  tbl[["quarter"]] <- NULL
   tbl[["year"]] <- NULL
   tbl <- dplyr::relocate(tbl, date)
   tbl
 }
 
-qdate <- function(tbl){
+mdate <- function(tbl) {
+  if (is.numeric(tbl$month)) {
+    tbl <- dplyr::mutate(tbl, date = paste(year, month, "01", sep = "-"))
+  } else {
+    if (!requireNamespace("tsibble", quietly = TRUE)) {
+      stop("Package \"tsibble\" needed for yearmonth format work. Please install it.", call. = FALSE)
+    }
+    tbl <- dplyr::mutate(
+      tbl,
+      date = paste(year, month),
+      date = tsibble::yearmonth(date)
+    )
+  }
+  dplyr::mutate(tbl,
+    date = as.Date(date),
+    date = lubridate::ceiling_date(date, unit = "month"),
+    date = lubridate::add_with_rollback(date, lubridate::days(-1))
+  )
+}
+
+abb_months <- function(tbl) {
+  dplyr::mutate(tbl,
+    month = stringr::str_trim(month),
+    month = stringr::str_sub(month, 1, 3),
+    month = stringr::str_to_title(month),
+    month = dplyr::case_when(
+      month == "Ene" ~ "Jan",
+      month == "Abr" ~ "Apr",
+      month == "Ago" ~ "Aug",
+      month == "Dic" ~ "Dec",
+      TRUE ~ month
+    )
+  )
+}
+
+
+ddate <- function(tbl) {
+  if (!is.numeric(tbl[["month"]])) {
+    tbl <- dplyr::mutate(tbl, month = match(month, month.abb))
+  }
   tbl %>%
     dplyr::mutate(
-      quarter = stringr::str_replace(quarter, "[eEjJ]-[mM]", "Q1"),
-      quarter = stringr::str_replace(quarter, "[eEjJaA]-[jJ]", "Q2"),
-      quarter = stringr::str_replace(quarter, "[eEjJ]-[sS]", "Q3"),
-      quarter = stringr::str_replace(quarter, "[eEjJoO]-[dD]", "Q4"),
+      date = paste(year, month, day, sep = "-"),
+      date = as.Date(date)
+    )
+}
+
+
+
+clean_date <- function(tbl) {
+  tbl <- tbl %>%
+    dplyr::mutate(
+      date = dplyr::case_when(
+        is.na(as.numeric(date)) ~ stringr::str_remove_all(stringr::str_remove_all(date, "\\."), "\\*"),
+        TRUE ~ date
+      ),
+      date = dplyr::case_when(
+        stringr::str_detect(tolower(date), "enero") ~ stringr::str_replace(tolower(date), "enero", "jan"),
+        stringr::str_detect(tolower(date), "ene") ~ stringr::str_replace(tolower(date), "ene", "jan"),
+        stringr::str_detect(tolower(date), "abril") ~ stringr::str_replace(tolower(date), "abril", "apr"),
+        stringr::str_detect(tolower(date), "abr") ~ stringr::str_replace(tolower(date), "abr", "apr"),
+        stringr::str_detect(tolower(date), "agosto") ~ stringr::str_replace(tolower(date), "agosto", "aug"),
+        stringr::str_detect(tolower(date), "ago") ~ stringr::str_replace(tolower(date), "ago", "aug"),
+        stringr::str_detect(tolower(date), "diciembre") ~ stringr::str_replace(tolower(date), "diciembre", "dec"),
+        stringr::str_detect(tolower(date), "dic") ~ stringr::str_replace(tolower(date), "dic", "dec"),
+        TRUE ~ date
+      ),
+      date = dplyr::case_when(
+        stringr::str_detect(
+          date,
+          stringr::regex("[a-z]{3}-")
+        ) ~ stringr::str_replace(
+          date,
+          stringr::regex("[a-z]{3}"),
+          paste0(
+            match(
+              stringr::str_to_title(stringr::str_extract(date, "[a-z]{3}")),
+              month.abb
+            )
+          )
+        ),
+        TRUE ~ date
+      )
+    )
+  if (date_format %in% c("%d-%m-%y", "%m-%y")) {
+    tbl <- tbl %>%
+      dplyr::mutate(
+        date = dplyr::case_when(
+          stringr::str_count(date, "-") == 1 & stringr::str_starts(date, "[0-9]") ~ paste0(1, "-", date),
+          TRUE ~ date
+        ),
+        date = dplyr::case_when(
+          stringr::str_count(date, "-") == 2 ~ as.character(difftime(as.Date(date, format = date_format), as.Date(origin), units = "days")),
+          TRUE ~ date
+        )
+      )
+  }
+  tbl %>%
+    dplyr::mutate(date = as.Date(as.numeric(date), origin = origin))
+}
+
+
+
+
+
+
+qdate <- function(tbl) {
+  tbl %>%
+    dplyr::mutate(
+      quarter = stringr::str_replace(quarter, stringr::regex("enero-marzo", ignore_case = T), "Q1"),
+      quarter = stringr::str_replace(quarter, stringr::regex("abril-junio", ignore_case = T), "Q2"),
+      quarter = stringr::str_replace(quarter, stringr::regex("julio-septiembre", ignore_case = T), "Q3"),
+      quarter = stringr::str_replace(quarter, stringr::regex("octubre-diciembre", ignore_case = T), "Q4"),
+      quarter = stringr::str_replace(quarter, "[eEjJ].+-.?[mM].+", "Q1"),
+      quarter = stringr::str_replace(quarter, "[eEjJaA].+-.?[jJ].+", "Q2"),
+      quarter = stringr::str_replace(quarter, "[eEjJ].+-.?[sS].+", "Q3"),
+      quarter = stringr::str_replace(quarter, "[eEjJoO].+-.?[dD].+", "Q4"),
+      quarter = stringr::str_replace(quarter, "IV", "Q4"),
+      quarter = stringr::str_replace(quarter, "III", "Q3"),
+      quarter = stringr::str_replace(quarter, "II", "Q2"),
+      quarter = stringr::str_replace(quarter, "I", "Q1"),
       date = tsibble::yearquarter(paste(year, quarter)),
       date = as.Date(date),
       date = lubridate::ceiling_date(date, unit = "quarter"),
       date = lubridate::add_with_rollback(date, lubridate::days(-1))
     ) %>%
-    dplyr::relocate(date) %>%
-    dplyr::select(-year, -quarter)
+    dplyr::relocate(date)
 }
