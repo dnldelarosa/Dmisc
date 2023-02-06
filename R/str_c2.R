2c5cd111222e62f5ef39aeda195249679e953303
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2023-02-05 23:23:24 -0400

    feat: str_c2

---
+++ R/str_c2.R
@@ -0,0 +1,23 @@
+#' str_c2: Concatenate Strings with Custom Last Delimiter
+#'
+#' This function concatenates multiple strings into a single string using a specified delimiter and provides the option to change the delimiter for the last pair of strings.
+#'
+#' @param ... strings to be concatenated.
+#' @param sep delimiter to be used to concatenate the strings.
+#' @param collapse default delimiter used in base str_c function.
+#' @param last_coll delimiter to be used for the last pair of strings.
+#'
+#' @return a single string that is the concatenation of all the input strings.
+#' @export
+#'
+#' @examples
+#' str_c2("A", "B", "C", "D", sep = ", ")
+#' str_c2("A", "B", "C", "D", sep = ", ", last_coll = " and ")
+str_c2 <- function(..., sep = "", collapse = NULL, last_coll = " y ") {
+  string <- stringr::str_c(..., sep = sep, collapse = collapse)
+  stringr::str_locate_all(string, collapse) %>%
+    `[[`(1) %>%
+    tail(1) -> last
+  stringr::str_sub(string, last[1], last[2]) <- last_coll
+  string
+}
