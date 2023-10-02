8cfc34706e3937297f01bb955bbb6367cbb71152
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2023-10-02 12:32:05 -0400

    feat: unpack and assign functions added

--- R/unpack_assign.R
+++ R/unpack_assign.R
@@ -1,4 +1,17 @@
 
+#' Unpack and assign values to specified names in an environment
+#' `r lifecycle::badge("experimental")`
+#'
+#' @param names A character vector specifying the names of variables to be assigned.
+#' @param values A list or vector containing the values to be assigned to the names.
+#' @param .envir The environment where the variables will be assigned. Defaults to the global environment.
+#' @param .warn Logical flag indicating whether to show a warning message. Defaults to TRUE.
+#'
+#' @return NULL. The function performs assignments in the specified environment.
+#' @export
+#'
+#' @examples
+#' c("x", "y") %<...% list(1, 2)
 `%<...%` <- function(names, values, .envir = .GlobalEnv, .warn = TRUE) {
   # Emitir una advertencia si 'warn' es TRUE
   if (.warn) {
@@ -34,6 +47,21 @@
 
 
 
+#' Wrapper around `%<...%` to unpack and assign values to specified names in an environment
+#' `r lifecycle::badge("experimental")`
+#'
+#' @param values A list or vector containing the values to be assigned to the names.
+#' @param names A character vector specifying the names of variables to be assigned.
+#' @param .envir The environment where the variables will be assigned. Defaults to the global environment.
+#' @param .warn Logical flag indicating whether to show a warning message. Defaults to TRUE.
+#'
+#' @return NULL. The function performs assignments in the specified environment.
+#' @export
+#'
+#' @examples
+#' values <- list(1, 2)
+#' names <- c("x", "y")
+#' values %...>% names
 `%...>%` <- function(values, names, .envir = .GlobalEnv, .warn = TRUE) {
   `%<...%`(names, values, .envir = .envir, .warn = .warn)
 }
